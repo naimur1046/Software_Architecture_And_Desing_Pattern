53b4359328ba939af1ba00ac5f0c57d97d09ccd7
naimur rahman rumel <88675051+naimur1046@users.noreply.github.com>
2023-05-22 23:29:19 +0600

    Create Ice_Asteroids .java

---
+++ Teacher_Content/Creatioal_Design_Pattern/Factory_Method/Astoraid_Example/concrete_product/Ice_Asteroids .java
@@ -0,0 +1,10 @@
+package creational_design_patterns.factory_method;
+
+public class Ice_Asteroids implements Asteroids
+{
+
+    @Override
+    public void show() {
+        System.out.println("Ice_Asteroids popped up");
+    }
+}
