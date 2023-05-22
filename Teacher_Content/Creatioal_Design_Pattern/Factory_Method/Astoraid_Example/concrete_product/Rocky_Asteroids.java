cfa1bc4f17c50e3149cf36cd86c798a3fcb520ed
naimur rahman rumel <88675051+naimur1046@users.noreply.github.com>
2023-05-22 23:34:49 +0600

    Create Rocky_Asteroids.java

---
+++ Teacher_Content/Creatioal_Design_Pattern/Factory_Method/Astoraid_Example/concrete_product/Rocky_Asteroids.java
@@ -0,0 +1,10 @@
+package creational_design_patterns.factory_method;
+
+public class Rocky_Asteroids implements Asteroids
+{
+
+    @Override
+    public void show() {
+        System.out.println("Rocky_Asteroids popped up");
+    }
+}
