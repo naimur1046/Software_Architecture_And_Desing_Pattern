88b82a31624ebc714b0797e74d76b41bc4223c8b
naimur rahman rumel <88675051+naimur1046@users.noreply.github.com>
2023-05-22 23:52:46 +0600

    Update client.java

--- Teacher_Content/Creatioal_Design_Pattern/Factory_Method/Astoraid_Example/client.java
+++ Teacher_Content/Creatioal_Design_Pattern/Factory_Method/Astoraid_Example/client.java
@@ -1,1 +1,15 @@
+package creational_design_patterns.factory_method;
+
+public class Client
+{
+    public static void main(String[] args) {
+        int level = (int)(Math.random() * 2) + 1;
+        AsteroidsFactory asteroidsFactory;
+        if(level==1) asteroidsFactory=new Level1AsteroidsFactory();
+        else asteroidsFactory=new Level2AsteroidsFactory();
+        int score = (int)(Math.random() * 2000);
+        Asteroids asteroids=asteroidsFactory.Create_Asteroids(score);
+        asteroids.show();
+    }
+}
 
