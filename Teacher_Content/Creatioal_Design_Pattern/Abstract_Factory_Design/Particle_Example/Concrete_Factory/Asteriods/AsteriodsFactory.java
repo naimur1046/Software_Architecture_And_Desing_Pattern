db7489c39e38e32be0095a7f7f51d5195740d1d4
naimur rahman rumel <88675051+naimur1046@users.noreply.github.com>
2023-05-23 09:46:21 +0600

    Update AsteriodsFactory.java

--- Teacher_Content/Creatioal_Design_Pattern/Abstract_Factory_Design/Particle_Example/Concrete_Factory/Asteriods/AsteriodsFactory.java
+++ Teacher_Content/Creatioal_Design_Pattern/Abstract_Factory_Design/Particle_Example/Concrete_Factory/Asteriods/AsteriodsFactory.java
@@ -1,1 +1,7 @@
 
+package creational_design_patterns.Abstrac_Factory_Method.Concrete_Factory.Asteroids;
+
+import creational_design_patterns.Abstrac_Factory_Method.Abstract_Factory.ObstacleFactory;
+
+public abstract class AsteriodsFactory extends ObstacleFactory {
+}
