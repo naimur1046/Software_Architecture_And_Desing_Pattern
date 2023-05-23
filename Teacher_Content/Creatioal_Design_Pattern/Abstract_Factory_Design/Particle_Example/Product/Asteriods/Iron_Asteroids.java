97eb100e9d47f9dad49e128e6b32c781fe3a429d
naimur rahman rumel <88675051+naimur1046@users.noreply.github.com>
2023-05-23 09:14:37 +0600

    Create Iron_Asteroids.java

---
+++ Teacher_Content/Creatioal_Design_Pattern/Abstract_Factory_Design/Particle_Example/Product/Asteriods/Iron_Asteroids.java
@@ -0,0 +1,10 @@
+package creational_design_patterns.Abstrac_Factory_Method.Product.Asteriods;
+
+public class Iron_Asteroids implements Asteriods
+{
+
+    @Override
+    public void show() {
+        System.out.println("Iron_Asteroids popped up");
+    }
+}
