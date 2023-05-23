7141bcaf81f15185fa8c87764e3211a6b86d1fa4
naimur rahman rumel <88675051+naimur1046@users.noreply.github.com>
2023-05-23 09:17:41 +0600

    Create Silicon_Asteroids.java

---
+++ Teacher_Content/Creatioal_Design_Pattern/Abstract_Factory_Design/Particle_Example/Product/Asteriods/Silicon_Asteroids.java
@@ -0,0 +1,10 @@
+package creational_design_patterns.Abstrac_Factory_Method.Product.Asteriods;
+
+public class Silicon_Asteroids implements Asteriods
+{
+
+    @Override
+    public void show() {
+        System.out.println("Silicon_Asteroids popped up");
+    }
+}
