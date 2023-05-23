861211b175fe119c6fe85944fee6db77cef6b412
naimur rahman rumel <88675051+naimur1046@users.noreply.github.com>
2023-05-23 09:16:17 +0600

    Create Rocky_Asteroids.java

---
+++ Teacher_Content/Creatioal_Design_Pattern/Abstract_Factory_Design/Particle_Example/Product/Asteriods/Rocky_Asteroids.java
@@ -0,0 +1,10 @@
+package creational_design_patterns.Abstrac_Factory_Method.Product.Asteriods;
+
+public class Rocky_Asteroids implements Asteriods
+{
+
+    @Override
+    public void show() {
+        System.out.println("Rocky_Asteroids popped up");
+    }
+}
