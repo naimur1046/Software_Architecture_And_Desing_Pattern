5a62c903b4d57e28e25b9a379d23ce8940933370
naimur rahman rumel <88675051+naimur1046@users.noreply.github.com>
2023-05-23 09:13:11 +0600

    Create Asteriods.java

---
+++ Teacher_Content/Creatioal_Design_Pattern/Abstract_Factory_Design/Particle_Example/Product/Asteriods/Asteriods.java
@@ -0,0 +1,7 @@
+package creational_design_patterns.Abstrac_Factory_Method.Product.Asteriods;
+
+import creational_design_patterns.Abstrac_Factory_Method.Product.Obstacle;
+
+public interface Asteriods extends Obstacle
+{
+}
