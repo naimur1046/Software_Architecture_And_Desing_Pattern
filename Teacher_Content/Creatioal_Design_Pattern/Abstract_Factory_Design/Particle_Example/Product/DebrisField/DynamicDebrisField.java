f78f1bdf40e3752c53ff295adf4fea2d325fdbb2
naimur rahman rumel <88675051+naimur1046@users.noreply.github.com>
2023-05-23 09:01:59 +0600

    Create DynamicDebrisField.java

---
+++ Teacher_Content/Creatioal_Design_Pattern/Abstract_Factory_Design/Particle_Example/Product/DebrisField/DynamicDebrisField.java
@@ -0,0 +1,10 @@
+package creational_design_patterns.Abstrac_Factory_Method.Product.DebrisField;
+
+public class DynamicDebrisField implements DebrisField
+{
+
+    @Override
+    public void show() {
+        System.out.println("Dynamic Debris Appeared");
+    }
+}
