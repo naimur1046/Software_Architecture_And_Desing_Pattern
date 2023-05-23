23b2736be79d86d44db80ba2121e2f85e319083a
naimur rahman rumel <88675051+naimur1046@users.noreply.github.com>
2023-05-23 09:05:10 +0600

    Create StaticDebrisField.java

---
+++ Teacher_Content/Creatioal_Design_Pattern/Abstract_Factory_Design/Particle_Example/Product/DebrisField/StaticDebrisField.java
@@ -0,0 +1,12 @@
+package creational_design_patterns.Abstrac_Factory_Method.Product.DebrisField;
+
+import creational_design_patterns.Abstrac_Factory_Method.Product.Obstacle;
+
+public class StaticDebrisField implements Obstacle
+{
+
+    @Override
+    public void show() {
+
+    }
+}
