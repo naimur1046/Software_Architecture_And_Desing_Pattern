284f3cc22353359c349dad4f11ca398361bc998b
naimur rahman rumel <88675051+naimur1046@users.noreply.github.com>
2023-05-23 08:59:38 +0600

    Create DebrisField.java

---
+++ Teacher_Content/Creatioal_Design_Pattern/Abstract_Factory_Design/Particle_Example/Product/DebrisField/DebrisField.java
@@ -0,0 +1,7 @@
+package creational_design_patterns.Abstrac_Factory_Method.Product.DebrisField;
+import creational_design_patterns.Abstrac_Factory_Method.Product.Obstacle;
+
+public interface DebrisField extends Obstacle 
+{
+
+}
