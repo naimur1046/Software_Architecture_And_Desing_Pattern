43904e8b94785dd992e9672a1f27d8a81f46cb29
naimur rahman rumel <88675051+naimur1046@users.noreply.github.com>
2023-07-20 11:20:07 +0600

    Update ChainOfResponsibility.java

--- Teacher_Content/Behavioral_Design_Patterns/ChainOfRespnosibility/ChainOfResponsibility.java
+++ Teacher_Content/Behavioral_Design_Patterns/ChainOfRespnosibility/ChainOfResponsibility.java
@@ -5,8 +5,11 @@ public class ChainOfResponsibility
     public static void main(String[] args) {
         PaymentHandler bank = new BankPaymentHandler();
         PaymentHandler credit_card = new CreditCardPaymentHandler();
-        PaymentHandler paypal = new CreditCardPaymentHandler();
+        PaymentHandler paypal = new PayPalPaymentHandler();
         bank.setNext(credit_card);
         credit_card.setNext(paypal);
+        bank.handlePayment(300);
+        bank.handlePayment(700);
+        bank.handlePayment(4000);
     }
 }
